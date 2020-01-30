a757f0c6275683598b4d74c0e24a599f576445fc
Masci, Richard (rx7322) <rx7322@us.att.com>
2020-01-30 11:07:08 -0500

    Changed license to MIT ATT

--- digicert.go
+++ digicert.go
@@ -1,22 +1,30 @@
 package main
- /*-
+
+/*-
  * ============LICENSE_START=======================================================
  * Author: Richard Masci
  * ================================================================================
- * Copyright (C) 2017 - 2020 AT&T Intellectual Property. All rights
- * 						reserved.
+ * Copyright (C) 2017 - 2020 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The MIT License (MIT)
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright <year> AT&T Intellectual Property. All other rights reserved.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * ============LICENSE_END=========================================================
  */
 // This is the digicert root that can be obtained from the internet. I put it here because
@@ -24,7 +32,6 @@ package main
 // stock OS SSL Root certs.
 
 //Other cert roots can be added here if required.
-
 
 func digicertRoot() string {
 	return `
